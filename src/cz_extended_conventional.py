9d63239895fe021db10f99adcf8294edadf92253
Lennart Rittel <33598633+lrittel@users.noreply.github.com>
2025-06-08 23:44:58 +0000

    feat: add a deps prefix
    In Commitizen's builtin configuration, there is no clear prefix for
    dependency updates.  Introduce a new prefix "deps" for this use
    case.

--- src/cz_extended_conventional.py
+++ src/cz_extended_conventional.py
@@ -1,5 +1,39 @@
-from commitizen.cz.conventional_commits import ConventionalCommitsCz
+from typing import override
+
+from commitizen.cz.conventional_commits.conventional_commits import (
+    ConventionalCommitsCz,
+)
+from commitizen.defaults import Questions
+
+# How long a line of the body or footer should be before wrapping.
+TEXT_WIDTH = 72
 
 
 class ExtendedConventionalCz(ConventionalCommitsCz):
-    pass
+    @override
+    def questions(self) -> Questions:
+        questions = list(super().questions())
+        prefix_question = questions[0]
+        assert (
+            prefix_question["name"] == "prefix"
+        ), "Expected the first question to be 'prefix'"
+        prefix_question["choices"].append(
+            {
+                "value": "deps",
+                "name": "deps: Dependency updates that do not change the behavior; use feat "
+                + "changes that cause user-facing changes.",
+                "key": "o",
+            }
+        )
+        return questions
+
+    @override
+    def schema_pattern(self) -> str:
+        PATTERN = (
+            r"(?s)"  # To explicitly make . match new line
+            r"(build|ci|deps|docs|feat|fix|perf|refactor|style|test|revert|bump)"  # type
+            r"(\(\S+\))?!?:"  # scope
+            r"( [^\n\r]+)"  # subject
+            r"((\n\n.*)|(\s*))?$"
+        )
+        return PATTERN
---
+++ tests/test_cz_cz_extended_conventional.py
@@ -0,0 +1,65 @@
+import pytest
+from commitizen import defaults
+from commitizen.config.base_config import BaseConfig
+
+from cz_extended_conventional import ExtendedConventionalCz
+
+
+# Taken from: https://github.com/commitizen-tools/commitizen/blob/c1884bddbeb6afa0af72c9526e18fac16c46e766/tests/conftest.py#L137
+@pytest.fixture()
+def config():
+    _config = BaseConfig()
+    _config.settings.update({"name": defaults.DEFAULT_SETTINGS["name"]})
+    return _config
+
+
+@pytest.fixture
+def extended_conventional(config):
+    return ExtendedConventionalCz(config)
+
+
+@pytest.mark.parametrize(
+    "prefix, expected",
+    [
+        # Test the new prefixes
+        ("deps", "deps: Subject of the commit"),
+        # Test existing prefixes
+        ("feat", "feat: Subject of the commit"),
+    ],
+)
+def test_prefixes(extended_conventional, prefix, expected):
+    answers = {
+        "prefix": prefix,
+        "scope": "",
+        "subject": "Subject of the commit",
+        "body": "",
+        "footer": "",
+        "is_breaking_change": False,
+    }
+    message = extended_conventional.message(answers)
+    assert message == expected
+
+
+@pytest.mark.parametrize(
+    "message, expected",
+    [
+        # Test the new prefixes
+        ("deps: Update dependencies", "Update dependencies"),
+        (
+            "deps!: Update important dependencies\n\nTest",
+            "Update important dependencies",
+        ),
+        (
+            "deps(component): Update dependencies in component",
+            "Update dependencies in component",
+        ),
+        # Test existing prefixes
+        ("feat: Add new feature", "Add new feature"),
+        # Test invalid prefix
+        ("invalid: This should not match", ""),
+        ("chore!: This should not match", ""),
+    ],
+)
+def test_schema_pattern(extended_conventional, message, expected):
+    result = extended_conventional.process_commit(message)
+    assert result == expected
